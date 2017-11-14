c926030995381719841d6b233d0435ecccc37d06
Alex Machado <amachado@sva.edu>
2017-11-14 18:10:41 -0500

    make buttons 1 and 2

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,20 @@
 (function () {
 
-  // your js goes here
+    const buttons = document.getElementsByTagName('button')
+
+    buttons[0].addEventListener('click', colorChange);
+
+    function colorChange (){
+      buttons[0].classList.toggle('button-danger');
+    }
+
+    buttons[1].addEventListener('click', youClicked);
+
+    function youClicked (){
+      buttons[1].innerHTML = "You clicked me!"
+    }
+
+    
+
 
 })();
