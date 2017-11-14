0fbda47d2bc2414d98222447c031268245b953d9
Alex Machado <amachado@sva.edu>
2017-11-14 18:16:31 -0500

    finish button activation

--- public/js/main.js
+++ public/js/main.js
@@ -11,10 +11,14 @@
     buttons[1].addEventListener('click', youClicked);
 
     function youClicked (){
-      buttons[1].innerHTML = "You clicked me!"
+      buttons[1].innerHTML = 'You clicked me!'
     }
 
-    
+    buttons[2].addEventListener('click', goBack);
+
+    function goBack (){
+      buttons[1].innerHTML = 'button two'
+    }
 
 
 })();
